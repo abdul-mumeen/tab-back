89eee37223b1d45acb692db56ac1a12813c4e195
Inumidun Amao <inumidun.amao@andela.com>
2019-02-13 13:39:53 +0100

    Add endpoint to sign JWT token

--- src/services/auth/routes/index.js
+++ src/services/auth/routes/index.js
@@ -4,6 +4,7 @@ const {createRoute} = require('../../../utils');
 module.exports = createRoute(
   [
     ['post', '/signin', 'signIn'],
-    ['post', '/signup', 'signUp']
+    ['post', '/signup', 'signUp'],
+    ['post', '/authenticate', 'authenticate']
   ],
 );
